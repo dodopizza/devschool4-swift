ae36fe6fc5d757968accf574cba32aa9f2418604
Aleksei Unshchikov <rgiggs@yandex.ru>
2018-07-18 10:36:15 +0300

    Create project

---
+++ Casino.Domain/Casino.Domain/Casino_Domain.h
@@ -0,0 +1,19 @@
+//
+//  Casino_Domain.h
+//  Casino.Domain
+//
+//  Created by Aleksei Unshchikov on 18.07.2018.
+//  Copyright © 2018 Dodo Pizza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Casino_Domain.
+FOUNDATION_EXPORT double Casino_DomainVersionNumber;
+
+//! Project version string for Casino_Domain.
+FOUNDATION_EXPORT const unsigned char Casino_DomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Casino_Domain/PublicHeader.h>
+
+
---
+++ Casino.Domain/Casino.DomainTests/Casino_DomainTests.swift
@@ -0,0 +1,30 @@
+//
+//  Casino_DomainTests.swift
+//  Casino.DomainTests
+//
+//  Created by Aleksei Unshchikov on 18.07.2018.
+//  Copyright © 2018 Dodo Pizza. All rights reserved.
+//
+
+import XCTest
+@testable import Casino_Domain
+
+class Casino_DomainTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    
+}
